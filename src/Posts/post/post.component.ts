a7dd0e12b3f9b3fd25b3f922fd0be20354cc5aec
sahar511 <sahararef@sahars-Air.gateway.sasktel.net>
2023-10-04 09:50:33 -0600

    Add home and post  component

---
+++ src/Posts/post/post.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+  @Input() imgSrc?: string = ''
+  @Input() title?: string = ''
+  @Input() id: number | undefined
+
+}
