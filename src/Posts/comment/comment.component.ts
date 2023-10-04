0375acdf3606aaef6dedee08fc253b49153466ae
sahar511 <sahararef@sahars-Air.gateway.sasktel.net>
2023-10-04 09:40:02 -0600

    Add comment component

---
+++ src/Posts/comment/comment.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent {
+  @Input() name: string = ''
+  @Input() text: string = ''
+}
